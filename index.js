58562e5f2864f26be430b5df1bc9866b19cdc3ef
dmuhia136 <89458252+dmuhia136@users.noreply.github.com>
2021-12-22 19:52:35 +0300

    a

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+require('ejs');
 // var exphbs = require('express-handlebars');
-const { engine } = require('express-handlebars');
+// const { engine } = require('express-handlebars');
 
 const messagebird = require('messagebird')('aoKCy9a6mQlOn1w9B4peyu9OLTpH4qmC');
-
-app.engine('handlebars', engine({ defaultLayout: 'main' }));
+// app.engine('handlebars', engine({ defaultLayout: 'main' }));
 // app.set('view-engine', 'handle-bars');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.engine('handlebars', engine({defaultLayout: "main"}));
-app.set('view engine', 'handlebars');
-app.set("views", "./views/layouts/");
-
-
+app.set('view engine', 'ejs');
+// app.set("views", "./views/layouts/");
 
 app.get('/', (req, res) => {
     res.render('step1');
 });
 
-app.post('step2', (req, res) => {
+app.post('/step2', (req, res) => {
     var number = req.body.number;
-
     messagebird.verify.create(number, {
         template: "Your verification code is %token."
     }), function (err, response) {
@@ -42,7 +39,7 @@ app.post('step2', (req, res) => {
     }
 });
 
-app.get('/', (req, res) => {
+app.get('/step3', (req, res) => {
     var id = req.body.id;
     var token = req.body.token;
 
@@ -54,7 +51,7 @@ app.get('/', (req, res) => {
             })
 
         }
-        else{
+        else {
             res.render('step3');
         }
     })
